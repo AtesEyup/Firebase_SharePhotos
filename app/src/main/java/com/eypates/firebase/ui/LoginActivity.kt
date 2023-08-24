6f9ac719fa86fbc2d5f4f045a6856885988a778b
Eyüp ATEŞ <eypates04@gmail.com>
2023-08-24 14:13:33 +0300

    BugFixed

--- app/src/main/java/com/eypates/firebase/ui/LoginActivity.kt
+++ app/src/main/java/com/eypates/firebase/ui/LoginActivity.kt
@@ -1,12 +1,13 @@
 package com.eypates.firebase.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.eypates.firebase.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.BuildConfig
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
@@ -67,5 +68,8 @@ class LoginActivity : AppCompatActivity() {
                 .addOnFailureListener { exception -> Toast.makeText(this, exception.localizedMessage, Toast.LENGTH_LONG).show() }
 
         }
+
+        layoutBnd.mainLblVersion.text = "V ${BuildConfig.VERSION_NAME}"
+
     }
 }
